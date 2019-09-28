41e94ac04478e78272b3a885d719d6849cf1f836
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-09-29 00:05:57 +0530

    storing data with path name

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+
 
 class ImageInput extends StatefulWidget {
   @override
@@ -17,6 +20,10 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       _storedImage = imageFile;
     });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+
   }
 
   @override
