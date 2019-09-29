4afe7418fa663178ef8b88d5a6417f9a7777a189
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-09-29 23:53:58 +0530

    inserting data from memory to db

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
+import 'dart:io';
+import '../helpers/db_helper.dart';
 
-class GreatPlaces with ChangeNotifier{
-   List<Place> _items = [];
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
 
-   List<Place> get items {
-     return [...items];
-   }
+  List<Place> get items {
+    return [..._items];
+  }
 
+  void addPlace(String pickedTitle, File pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('places', {
+      'id' : newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
 }
