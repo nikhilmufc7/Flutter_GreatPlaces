26476faf16f0f7ca95f002d2eceff67d4b30142f
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-10-03 20:58:02 +0530

    place details screen now shows map of the place too along with image :fire:

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'place_detail_screen.dart';
 import './add_place_screen.dart';
 import '../providers/great_places.dart';
 
@@ -44,7 +44,8 @@ class PlacesListScreen extends StatelessWidget {
                           title: Text(greatPlaces.items[i].title),
                           subtitle: Text(greatPlaces.items[i].location.address),
                           onTap: () {
-                            // Go to detail page ...
+                            Navigator.of(context).pushNamed(PlaceDetailScreen.routeName,
+                            arguments: greatPlaces.items[i].id);
                           },
                         ),
                       ),
