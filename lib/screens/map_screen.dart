fbc162ee51e99a44323eb6dc8e69ac7390d4469b
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-10-03 20:57:25 +0530

    added markers for place detail screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -51,10 +51,13 @@ class _MapScreenState extends State<MapScreen> {
           zoom: 16,
         ),
         onTap: widget.isSelecting ? _selectLocation : null,
-        markers: _pickedLocation == null ? null : {
+        markers: _pickedLocation == null && widget.isSelecting ? null : {
           Marker(
             markerId: MarkerId('M1'),
-            position: _pickedLocation,
+            position: _pickedLocation ?? LatLng(
+                widget.initialLocation.latitude,
+                widget.initialLocation.longitude
+            ),
           ),
         },
       ),
